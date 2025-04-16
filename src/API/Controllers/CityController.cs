a609228c780fb897fedef89631e41d8cf0faafd4
matheuscanutoVegaTech <matheus.canuto@vegatech.com.br>
2025-04-16 14:21:59 -0300

    estrutura padrão

---
+++ src/API/Controllers/CityController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CityController : ControllerBase
+{
+    private readonly ApplicationDbContext _dbContext;
+    public CityController(ApplicationDbContext context)
+    {
+        _dbContext = context;
+    }
+    // GET: NotificationController
+    [HttpGet]
+    public async Task<ActionResult> Index()
+    {
+        await _dbContext.Cities.AddAsync(new City
+        {
+            Name = "São Paulo",
+            CountryId = 5
+        });
+
+        await _dbContext.SaveChangesAsync();
+
+        var result = await _dbContext.Cities.ToListAsync();
+        return Ok(result);
+    }
+
+}
---
+++ src/API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using API.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<City> Cities { get; set; }
+}
