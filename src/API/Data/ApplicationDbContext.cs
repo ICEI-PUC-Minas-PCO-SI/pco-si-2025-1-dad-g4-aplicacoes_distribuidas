c8ec84a66be0f906c658eccef4414da46b7d575d
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-05-23 19:22:53 -0300

    DbContext

--- src/API/Data/ApplicationDbContext.cs
+++ src/API/Data/ApplicationDbContext.cs
@@ -11,4 +11,12 @@ namespace API.Data
         public DbSet<City> Cities { get; set; }
         public DbSet<Payments> Payments { get; set; }
     }
+namespace API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Notifications> Notifications { get; set; }
 }
