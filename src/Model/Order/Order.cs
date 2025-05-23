bf9ac5ec97b86e8cda7096b9396ce727e44f2f69
Ana Clara Marçal <127258897+anamarcal@users.noreply.github.com>
2025-05-23 20:31:09 -0300

    Update Order.cs

--- src/Model/Order/Order.cs
+++ src/Model/Order/Order.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Model.Order
 {
-    class Order
+    public class Order
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Customer { get; set; }  // Nome do cliente
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [Required]
+        public decimal Total { get; set; }
+
+        public string Status { get; set; } = "Pendente";
     }
 }
