af97624300b478145550d5a7a95f115c9f307413
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-05-25 21:04:26 -0300

    sendemail

--- src/API/Data/ApplicationDbContext.cs
+++ src/API/Data/ApplicationDbContext.cs
@@ -1,22 +1,14 @@
 ﻿using API.Controllers;
 using Microsoft.EntityFrameworkCore;
-using Model.Payments; 
+using Model.Payments;
 
 namespace API.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
         public DbSet<City> Cities { get; set; }
         public DbSet<Payments> Payments { get; set; }
+        public DbSet<Notification> Notification { get; set; }
     }
-namespace API.Data;
-
-public class ApplicationDbContext : DbContext
-{
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        
-    public DbSet<City> Cities { get; set; }
-    public DbSet<Notifications> Notifications { get; set; }
 }
