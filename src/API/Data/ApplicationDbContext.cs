ea0c4eb76582dadb8d07617c178c1ea28c00a426
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-05-30 23:57:25 -0300

    removendoTabelaCities

--- src/API/Data/ApplicationDbContext.cs
+++ src/API/Data/ApplicationDbContext.cs
@@ -10,7 +10,6 @@ namespace API.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<City> Cities { get; set; }
         public DbSet<Payments> Payments { get; set; }
         public DbSet<Products> Products { get; set; }
         public DbSet<Notification> Notification { get; set; }
