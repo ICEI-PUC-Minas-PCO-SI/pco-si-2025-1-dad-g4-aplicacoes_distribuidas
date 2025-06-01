a03799f2862775291a17558ed047b3adc1922433
barbara-xsb <barbarax2503@gmail.com>
2025-06-01 10:21:49 -0300

    autenticacao quase completa

--- src/API/Data/ApplicationDbContext.cs
+++ src/API/Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
 ﻿using API.Controllers;
 using Microsoft.EntityFrameworkCore;
 using Model.Autentication;
+using Model.Order;
 using Model.Payments;
+using Model.Products;
 
 namespace API.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<City> Cities { get; set; }
         public DbSet<Payments> Payments { get; set; }
         public DbSet<Notification> Notification { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Products> Products { get; set; }
         public DbSet<Autentication> Autentication { get; set; }
 
     }
