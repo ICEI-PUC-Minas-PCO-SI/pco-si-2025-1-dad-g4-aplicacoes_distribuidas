42e64bf86dc3b1d6af473d83f7d2df0b3dcd8d41
Matheuscanuto07 <matheushenriquecanuto77@gmail.com>
2025-06-01 17:25:11 -0300

    DefaultValues

--- src/Model/DefaultValues.cs
+++ src/Model/DefaultValues.cs
@@ -7,7 +7,7 @@ namespace Model
         public static class StatusPedido
         {
             public static string Processando = "Processando";
-            public static string EmSeparacao = "EmSeparacao";
+            public static string EmSeparacao = "EmSeparacao"; 
             public static string EmRotaDeEntrega = "EmRotaDeEntrega";
             public static string Concluido = "Concluido";
             public static string PedidoCancelado = "Cancelado";
